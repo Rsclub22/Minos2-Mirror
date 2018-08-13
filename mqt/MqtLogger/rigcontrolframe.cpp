e79572f3514c334d942c324c1cd1152f1dbd0f84
Mike Goodey <mike@goodey.org.uk>
2018-08-13 15:57:07 +0100

    #311, #312, #319 - try to improve on locators and bearings in memories

--- mqt/MqtLogger/rigcontrolframe.cpp
+++ mqt/MqtLogger/rigcontrolframe.cpp
@@ -594,7 +594,7 @@ void RigControlFrame::getDetails(memoryData::memData &logData)
 
     logData.callsign = sc.cs.fullCall.getValue();
     logData.freq = curFreq;
-    logData.locator = sc.loc.loc.getValue();
+    logData.locator = sc.loc.loc.getValue().trimmed();
     logData.mode = curMode;
 
     QStringList dt = dtg( true ).getIsoDTG().split('T');
@@ -604,7 +604,14 @@ void RigControlFrame::getDetails(memoryData::memData &logData)
 
     //logData.bearing = sc.bearing;
 
-    logData.bearing = tslf->getBearingFrmQSOLog();
+    if (sc.loc.loc.getValue().trimmed().isEmpty())
+    {
+        logData.bearing = tslf->getCurrentBearing();
+    }
+    else
+    {
+        logData.bearing = tslf->getBearingFrmQSOLog();
+    }
     // load log data into memory
 }
 void RigControlFrame::getRigDetails(memoryData::memData &m)
