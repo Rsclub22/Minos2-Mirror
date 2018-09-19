bc4b61a4583a30eee7b4089397b56db405c09e95
Mike Goodey <mike@goodey.org.uk>
2018-09-19 18:12:27 +0100

    Unused values

--- mqt/MqtBandmap/rigcontrol.h
+++ mqt/MqtBandmap/rigcontrol.h
@@ -7,11 +7,6 @@
 
 #include <hamlib/rotator.h>
 
-#define MAXCONFLEN 128
-
-extern "C" int write_block(hamlib_port_t *p, const char *txbuffer, size_t count);
-extern "C" int read_block(hamlib_port_t *p, char *rxbuffer, size_t count);
-
 bool model_Sort(const rig_caps *caps1,const rig_caps *caps2);
 
 
