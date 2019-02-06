e8730cc02ab92663f7ae1c484ec24f88abeec4eb
Mike Goodey <mike@goodey.org.uk>
2019-02-06 11:33:29 +0000

    Why am I getting a pip when I've turned it off?

--- mqt/MqtKeyer/keyers.cpp
+++ mqt/MqtKeyer/keyers.cpp
@@ -324,6 +324,7 @@ void setPipEnabled( bool val )
    if ( currentKeyer )
    {
       currentKeyer->kconf.enablePip = val;
+      trace(QString("Pip set to ").arg(val?"true":"false"));
    }
 }
 
@@ -507,6 +508,7 @@ bool commonKeyer::linesModeChanged( int state )
       trace( "linesModeChanged(" + QString::number( state ) + ")" );
    }
    linesMode = static_cast<LineModes>(state);
+   trace("lineMode is now " + lineModeStrings[linesMode]);
    return true;
 }
 
