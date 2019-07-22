b02c4f79797cb194089f7da35132c0a114606b29
davidg <davidg@balharrie.me>
2019-07-22 19:15:02 +0100

    commit

--- mqt/MqtLogger/bandmapview.cpp
+++ mqt/MqtLogger/bandmapview.cpp
@@ -19,10 +19,9 @@
 
 
 
-BandmapView::BandmapView(QWidget *parent) : QWidget(parent)
+BandmapView::BandmapView(QWidget *parent) : QAbstractItemView(parent)
 {
-    bandmap = qobject_cast<Bandmap*>(parent);
-    Q_ASSERT(bandmap);
+
     setFocusPolicy((Qt::WheelFocus));
     setMinimumSize(minimumSizeHint());
 
@@ -57,8 +56,75 @@ BandmapView::BandmapView(QWidget *parent) : QWidget(parent)
 
 }
 
+void BandmapView::setModel(QAbstractItemModel *model)
+{
+    QAbstractItemView::setModel(model);
+    //hashIsDirty = true;
+}
+
+QRect BandmapView::visualRect(const QModelIndex &index) const
+{
+    QRect rect;
+    if (index.isValid())
+        rect = viewportRectForRow(index.row()).toRect();
+    return rect;
+}
+
+QRectF BandmapView::viewportRectForRow(int row) const
+{
+    calculateRectsIfNecessary();
+    QRectF rect = rectForRow.value(row).toRect();
+    if (!rect.isValid())
+        return rect;
+    return QRectF(rect.x() - horizontalScrollBar()->value(),
+                  rect.y() - verticalScrollBar()->value(),
+                  rect.width(), rect.height());
+}
+
+void BandmapView::scrollTo(const QModelIndex &index,
+                             QAbstractItemView::ScrollHint)
+{
+    QRect viewRect = viewport()->rect();
+    QRect itemRect = visualRect(index);
+
+    if (itemRect.left() < viewRect.left())
+        horizontalScrollBar()->setValue(horizontalScrollBar()->value()
+                + itemRect.left() - viewRect.left());
+    else if (itemRect.right() > viewRect.right())
+        horizontalScrollBar()->setValue(horizontalScrollBar()->value()
+                + qMin(itemRect.right() - viewRect.right(),
+                       itemRect.left() - viewRect.left()));
+    if (itemRect.top() < viewRect.top())
+        verticalScrollBar()->setValue(verticalScrollBar()->value() +
+                itemRect.top() - viewRect.top());
+    else if (itemRect.bottom() > viewRect.bottom())
+        verticalScrollBar()->setValue(verticalScrollBar()->value() +
+                qMin(itemRect.bottom() - viewRect.bottom(),
+                     itemRect.top() - viewRect.top()));
+    viewport()->update();
+}
 
 
+QModelIndex BandmapView::indexAt(const QPoint &point_) const
+{
+    QPoint point(point_);
+    point.rx() += horizontalScrollBar()->value();
+    point.ry() += verticalScrollBar()->value();
+    calculateRectsIfNecessary();
+    QHashIterator<int, QRectF> i(rectForRow);
+    while (i.hasNext()) {
+        i.next();
+        if (i.value().contains(point))
+            return model()->index(i.key(), 0, rootIndex());
+    }
+    return QModelIndex();
+}
+
+void BandmapView::dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight)
+{
+    hashIsDirty = true;
+    QAbstractItemView::dataChanged(topLeft, bottomRight);
+}
 
 QSize BandmapView::minimumSizeHint() const
 {
@@ -67,7 +133,51 @@ QSize BandmapView::minimumSizeHint() const
 
 }
 
+void BandmapView::rowsInserted(const QModelIndex &parent, int start, int end)
+{
+    hashIsDirty = true;
+    QAbstractItemView::rowsInserted(parent, start, end);
+}
 
+void BandmapView::rowsAboutToBeRemoved(const QModelIndex &parent,
+                                         int start, int end)
+{
+    hashIsDirty = true;
+    QAbstractItemView::rowsAboutToBeRemoved(parent, start, end);
+}
+
+QModelIndex BandmapView::moveCursor(
+        QAbstractItemView::CursorAction cursorAction,
+        Qt::KeyboardModifiers)
+{
+    QModelIndex index = currentIndex();
+    if (index.isValid()) {
+        if ((cursorAction == MoveLeft && index.row() > 0) ||
+            (cursorAction == MoveRight &&
+             index.row() + 1 < model()->rowCount())) {
+            const int offset = (cursorAction == MoveLeft ? -1 : 1);
+            index = model()->index(index.row() + offset,
+                                   index.column(), index.parent());
+        }
+        else if ((cursorAction == MoveUp && index.row() > 0) ||
+                 (cursorAction == MoveDown &&
+                  index.row() + 1 < model()->rowCount())) {
+            QFontMetrics fm(font());
+            const int RowHeight = (fm.height() + ExtraHeight) *
+                                  (cursorAction == MoveUp ? -1 : 1);
+            QRect rect = viewportRectForRow(index.row()).toRect();
+            QPoint point(rect.center().x(),
+                         rect.center().y() + RowHeight);
+            while (point.x() >= 0) {
+                index = indexAt(point);
+                if (index.isValid())
+                    break;
+                point.rx() -= fm.width("n");
+            }
+        }
+    }
+    return index;
+}
 void BandmapView::bandmapResize(int _height)
 {
     int height = _height;
@@ -79,6 +189,127 @@ void BandmapView::bandmapResize(int _height)
 
 
 }
+
+int TiledListView::horizontalOffset() const
+{
+    return horizontalScrollBar()->value();
+}
+
+
+int TiledListView::verticalOffset() const
+{
+    return verticalScrollBar()->value();
+}
+
+
+void TiledListView::scrollContentsBy(int dx, int dy)
+{
+    scrollDirtyRegion(dx, dy);
+    viewport()->scroll(dx, dy);
+}
+
+
+void TiledListView::setSelection(const QRect &rect,
+        QFlags<QItemSelectionModel::SelectionFlag> flags)
+{
+    QRect rectangle = rect.translated(horizontalScrollBar()->value(),
+            verticalScrollBar()->value()).normalized();
+    calculateRectsIfNecessary();
+    QHashIterator<int, QRectF> i(rectForRow);
+    int firstRow = model()->rowCount();
+    int lastRow = -1;
+    while (i.hasNext()) {
+        i.next();
+        if (i.value().intersects(rectangle)) {
+            firstRow = firstRow < i.key() ? firstRow : i.key();
+            lastRow = lastRow > i.key() ? lastRow : i.key();
+        }
+    }
+    if (firstRow != model()->rowCount() && lastRow != -1) {
+        QItemSelection selection(
+                model()->index(firstRow, 0, rootIndex()),
+                model()->index(lastRow, 0, rootIndex()));
+        selectionModel()->select(selection, flags);
+    }
+    else {
+        QModelIndex invalid;
+        QItemSelection selection(invalid, invalid);
+        selectionModel()->select(selection, flags);
+    }
+
+}
+
+
+QRegion TiledListView::visualRegionForSelection(
+        const QItemSelection &selection) const
+{
+    QRegion region;
+    foreach (const QItemSelectionRange &range, selection) {
+        for (int row = range.top(); row <= range.bottom(); ++row) {
+            for (int column = range.left(); column < range.right();
+                 ++column) {
+                QModelIndex index = model()->index(row, column,
+                                                   rootIndex());
+                region += visualRect(index);
+            }
+        }
+    }
+    return region;
+}
+
+
+void TiledListView::paintEvent(QPaintEvent*)
+{
+    QPainter painter(viewport());
+    painter.setRenderHints(QPainter::Antialiasing|
+                           QPainter::TextAntialiasing);
+    for (int row = 0; row < model()->rowCount(rootIndex()); ++row) {
+        QModelIndex index = model()->index(row, 0, rootIndex());
+        QRectF rect = viewportRectForRow(row);
+        if (!rect.isValid() || rect.bottom() < 0 ||
+            rect.y() > viewport()->height())
+            continue;
+        QStyleOptionViewItem option = viewOptions();
+        option.rect = rect.toRect();
+        if (selectionModel()->isSelected(index))
+            option.state |= QStyle::State_Selected;
+        if (currentIndex() == index)
+            option.state |= QStyle::State_HasFocus;
+        itemDelegate()->paint(&painter, option, index);
+        paintOutline(&painter, rect);
+    }
+}
+
+void TiledListView::resizeEvent(QResizeEvent*)
+{
+    hashIsDirty = true;
+    calculateRectsIfNecessary();
+    updateGeometries();
+}
+
+
+void TiledListView::updateGeometries()
+{
+    QFontMetrics fm(font());
+    const int RowHeight = fm.height() + ExtraHeight;
+    horizontalScrollBar()->setSingleStep(fm.width("n"));
+    horizontalScrollBar()->setPageStep(viewport()->width());
+    horizontalScrollBar()->setRange(0,
+            qMax(0, idealWidth - viewport()->width()));
+    verticalScrollBar()->setSingleStep(RowHeight);
+    verticalScrollBar()->setPageStep(viewport()->height());
+    verticalScrollBar()->setRange(0,
+            qMax(0, idealHeight - viewport()->height()));
+}
+
+
+void TiledListView::mousePressEvent(QMouseEvent *event)
+{
+    QAbstractItemView::mousePressEvent(event);
+    setCurrentIndex(indexAt(event->pos()));
+}
+
+
 
 
 QSize BandmapView::sizeHint() const
--- mqt/MqtLogger/bandmapview.h
+++ mqt/MqtLogger/bandmapview.h
@@ -12,27 +12,50 @@
 #include "bandmapfreqdial.h"
 #include "textmarker.h"
 
-class BandmapView : public QWidget
+class BandmapView : public QAbstractItemView
 {
     Q_OBJECT
 public:
     explicit BandmapView(QWidget *parent = nullptr);
-    QSize minimumSizeHint() const override;
-    QSize sizeHint() const override;
+    void setModel(QAbstractItemModel *model) override;
+    QRect visualRect(const QModelIndex &index) const override;
+    void scrollTo(const QModelIndex &index,
+                  QAbstractItemView::ScrollHint) override;
+    QModelIndex indexAt(const QPoint &point_) const override;
+
+
+    //QSize minimumSizeHint() const override;
+    //QSize sizeHint() const override;
 
     void setFreq(double f);
+protected slots:
+    void dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight);
+    void rowsInserted(const QModelIndex &parent, int start, int end) override;
+    void rowsAboutToBeRemoved(const QModelIndex &parent, int start,
+                              int end) override;
+    void updateGeometries() override;
+
+
+
 signals:
     void clicked(const QModelIndex&);
 
 protected:
-    bool eventFilter(QObject *target, QEvent *event) override;
+protected:
+    QModelIndex moveCursor(QAbstractItemView::CursorAction cursorAction, Qt::KeyboardModifiers modifiers) override;
+    bool isIndexHidden(const QModelIndex&) const override{ return false; }
+    int horizontalOffset() const override;
+    int verticalOffset() const override;
+    void scrollContentsBy(int dx, int dy) override;
+    void setSelection(const QRect &rect, QFlags<QItemSelectionModel::SelectionFlag> flags) override;
+    QRegion visualRegionForSelection(const QItemSelection &selection) const override;
+
+    void paintEvent(QPaintEvent*) override;
+    void resizeEvent(QResizeEvent*) override;
     void mousePressEvent(QMouseEvent *event) override;
-    void keyPressEvent(QKeyEvent *event) override;
-    void paintEvent(QPaintEvent *event) override;
-    //void wheelEvent(QWheelEvent *event) override;
 
 
-    void resizeEvent(QResizeEvent *) override;
+
 
 public slots:
 
@@ -40,6 +63,7 @@ public slots:
 
 private slots:
     void bandmapResize(int);
+
 private:
 
     Bandmap *bandmap;
