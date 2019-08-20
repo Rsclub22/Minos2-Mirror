485c707823deb5e722853cd5f678a7d47c815ec1
davidg <davidg@balharrie.me>
2019-08-20 19:05:05 +0100

    commit

---
+++ mqt/MqtBase/freqmodebandplan.cpp
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////
+// $Id$
+//
+// PROJECT NAME 		Minos Amateur Radio Control and Logging System
+//                      Cluster Mode/Bandplan
+// Copyright        (c) D. G. Balharrie M0DGB/G8FKH 2019
+//
+///
+//
+//
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+#include "freqmodebandplan.h"
+
+
+// parses a json file to get mode frequencies for each band or "legal" operating frequencies for each band
+
+freqModeBandPlan::freqModeBandPlan()
+{
+
+}
---
+++ mqt/MqtBase/freqmodebandplan.h
@@ -0,0 +1,41 @@
+#ifndef FREQMODEBANDPLAN_H
+#define FREQMODEBANDPLAN_H
+
+#include "base_pch.h"
+#include "clustercommon.h"
+
+
+
+class ModeFreqDetail
+{
+    //QString band;
+    //QString mode;
+public:
+
+    QList<QList<double>> freq;
+
+};
+
+
+
+class freqModeBandPlan
+{
+public:
+    freqModeBandPlan();
+
+    bool loadFile(QString filename);
+
+    int confirmMode(QString &band, QString &mode, double freq);
+    bool modeExists(QString &band, QString &mode);
+
+private:
+
+    bool readFile(QString f);
+    QMap<QString, QMap<QString, ModeFreqDetail> > bandModeFreqList;
+    QMap<QString, ModeFreqDetail> modeFreqList;
+
+
+
+};
+
+#endif // FREQMODEBANDPLAN_H
