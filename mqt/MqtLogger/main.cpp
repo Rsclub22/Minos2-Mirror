34545e50bafb0ec5cd86856d45dce8f5fc29e39c
Mike Goodey <mike@goodey.org.uk>
2019-09-07 21:41:41 +0100

    Take out msvc leakage code

--- mqt/MqtLogger/main.cpp
+++ mqt/MqtLogger/main.cpp
@@ -6,74 +6,14 @@
 #include "AppStartup.h"
 
 #ifdef _MSC_VER
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
 #endif // _MSC_VER
 
 
-#if defined(_MSC_VER)
-
-// Code to display the memory leak report
-// We use a custom report hook to filter out Qt's own memory leaks
-// Credit to Andreas Schmidts - http://www.schmidt-web-berlin.de/winfig/blog/?p=154
-
-static _CRT_REPORT_HOOK prevHook;
-
-int customReportHook(int /* reportType */, char* message, int* /* returnValue */)
-{
-    // This function is called several times for each memory leak.
-    // Each time a part of the error message is supplied.
-    // This holds number of subsequent detail messages after
-    // a leak was reported
-    const int numFollowupDebugMsgParts = 2;
-    static bool ignoreMessage = false;
-    static int debugMsgPartsCount = 0;
-
-    // check if the memory leak reporting starts
-    if ((strncmp(message, "Detected memory leaks!\n", 10) == 0)
-        || ignoreMessage)
-    {
-        // check if the memory leak reporting ends
-        if (strncmp(message, "Object dump complete.\n", 10) == 0)
-        {
-            _CrtSetReportHook(prevHook);
-            ignoreMessage = false;
-        }
-        else
-            ignoreMessage = true;
-
-        // something from our own code?
-        if (strstr(message, ".cpp") == nullptr)
-        {
-            if (debugMsgPartsCount++ < numFollowupDebugMsgParts)
-                // give it back to _CrtDbgReport() to be printed to the console
-                return false;
-            else
-                return true;  // ignore it
-        }
-        else
-        {
-            debugMsgPartsCount = 0;
-            // give it back to _CrtDbgReport() to be printed to the console
-            return false;
-        }
-    }
-    else
-        // give it back to _CrtDbgReport() to be printed to the console
-        return true;
-}
-
-#endif
-
 int main(int argc, char *argv[])
 {
-#if defined(_MSC_VER)
-//    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF /*| _CRTDBG_CHECK_ALWAYS_DF*/);
-//    prevHook = _CrtSetReportHook(customReportHook);
-//     _CrtSetBreakAlloc(1312120); // Use this line to break at the nth memory allocation
-#endif
-
     int appError = 1;
     {
         SingleApplication a( QString("MinosLogger"), argc, argv);
@@ -125,10 +65,5 @@ int main(int argc, char *argv[])
         }
     }
 
-#if defined(_MSC_VER)
-    // Once the app has finished running and has been deleted,
-    // we run this command to view the memory leaks:
-//    _CrtDumpMemoryLeaks();
-#endif
     return appError;
 }
