7e0bc9ecc975cb1e6ca9a9039ceabc121b591bce
davidg <davidg@balharrie.me>
2019-09-08 19:04:28 +0100

    commit

--- mqt/MqtLogger/bandmapfreqdial.cpp
+++ mqt/MqtLogger/bandmapfreqdial.cpp
@@ -210,10 +210,10 @@ void BandmapFreqDial::drawScale(QPainter *painter, qint32 frequency, int scaleHe
        scaleStartFreq =  ((scaleStartFreq + 2) / 10) * 10;
     }
 
-//    if (scaleStartFreq < 144150)  //// ************************this needs to be the lower limit of the band
-//    {
-//        scaleStartFreq = 144150;
-//    }
+    if (scaleStartFreq < 144150)  //// ************************this needs to be the lower limit of the band
+    {
+        scaleStartFreq = 144150;
+    }
     scaleEndFreq = scaleStartFreq + freqRange;
 */
     trace(QString("bandmapFreqDial: scale startFreq = %1").arg(scaleStartFreq));
