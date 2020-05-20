2b74c93401b9786e4d89bca8fbbf8df4065c7b9c
Mike Goodey <mike@goodey.org.uk>
2020-05-20 11:53:07 +0100

    Rotator crash when can't get library version

--- mqt/MqtRotator/rotatormainwindow.cpp
+++ mqt/MqtRotator/rotatormainwindow.cpp
@@ -2294,7 +2294,17 @@ void RotatorMainWindow::dumpRotatorToTraceLog()
     {
         trace("*** Antenna Updated ***");
         trace(QString("App Instance Name  = %1").arg(appName));
-        trace(QString("Hamlib Version = %1").arg(rotator->gethamlibVersion()));
+        if (rotator != nullptr)
+        {
+            trace(QString("Library Version = %1").arg(rotator->getRotLibVersion()));
+
+        }
+        else
+        {
+            trace(QString("Unable to determine library version at this time!"));
+
+        }
+
         trace(QString("Antenna Name = %1").arg(setupAntenna->currentAntenna.antennaName));
         trace(QString("Antenna Number = %1").arg(setupAntenna->currentAntenna.antennaNumber));
         trace(QString("Rotator Model = %1").arg(setupAntenna->currentAntenna.rotatorModel));
