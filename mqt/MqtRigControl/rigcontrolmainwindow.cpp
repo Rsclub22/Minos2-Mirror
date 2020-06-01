daeb4365bf00ae1b49d00df28ea00ffdff5e47c0
davidg <davidg@balharrie.me>
2020-06-01 20:53:15 +0100

    reinstate the volmult in hamlig getVolume()

--- mqt/MqtRigControl/rigcontrolmainwindow.cpp
+++ mqt/MqtRigControl/rigcontrolmainwindow.cpp
@@ -2890,6 +2890,7 @@ int RigControlMainWindow::getVolume(VFO vfo)
         int vol = 0;
         //value.f = value.f * VOLMULT;
         //vol = qRound(value.f);
+        value = value * VOLMULT;
         vol = qRound(value);
         if (vol > 200)
         {
