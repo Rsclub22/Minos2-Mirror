bf99d01958e50b06e7fda96d9c3a04b344e58c65
Mike Goodey <mike@goodey.org.uk>
2020-07-29 11:42:18 +0100

    ADIF getting wrong freq when no rig control; also microwaves freqs were wrong

--- mqt/MqtLogger/LoggerContacts.cpp
+++ mqt/MqtLogger/LoggerContacts.cpp
@@ -532,7 +532,7 @@ QString ContestContact::getADIFLine()
 
 
     QString cb;
-    long txfreq = contest->getAdifFreqBand(frequency.getValue(), cb);
+    double txfreq = contest->getAdifFreqBand(frequency.getValue(), cb);
     outstr += makeADIFField( "BAND", cb );
 
     double dfreq = txfreq/1000000.0;  // MHz
