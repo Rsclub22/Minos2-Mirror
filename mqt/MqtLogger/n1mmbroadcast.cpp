844c44e6e86076980c2c6736d91f5892b9ecd9c1
Mike Goodey <mike@goodey.org.uk>
2020-12-31 11:29:55 +0000

    Try to get MGM modes reported correctly to (e.g.) LOG4OM and in ADIF

--- mqt/MqtLogger/n1mmbroadcast.cpp
+++ mqt/MqtLogger/n1mmbroadcast.cpp
@@ -235,6 +235,14 @@ QString N1MMBroadcast::genContactStanza(QString type, BaseContestLog *b, QShared
 
     QString continent = (tct->ctryMult?tct->ctryMult->getContinent():QString());
 
+    QString mode = tct->mode.getValue();
+    QString mgmSubmode = tct->mgmSubmode.getValue();
+    if ( (mode.compare( hamlibData::MGM) == 0 || mode.compare( "MFSK") == 0) && !mgmSubmode.trimmed().isEmpty())
+    {
+         mode = mgmSubmode;
+    }
+
+
     QString xml = QString("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
                   + "<" + type + ">\n"
                    + makeTag("app", "Minos")
