b96b9c09eeeaf3c365cfaaf03062e1e7b91d5f75
Mike Goodey <mike@goodey.org.uk>
2021-01-29 17:24:20 +0000

    Crashes on actual machine

--- mqt/MqtLogger/tsinglelogframe.cpp
+++ mqt/MqtLogger/tsinglelogframe.cpp
@@ -375,8 +375,17 @@ void TSingleLogFrame::clearScreenLayout()
     // BUT on contest creation, the contest address may change, so clear the contest
 
     LoggerContestLog *ct = dynamic_cast<LoggerContestLog *>( getContest() );
-    QString msg = ct->name.getValue() + " uuid " + ct->uuid;
-    traceMsg("clearScreenLayout starts for " + msg);
+    QString msg;
+    if (ct != nullptr)
+    {
+        msg = ct->name.getValue() + " uuid " + ct->uuid;
+        traceMsg("clearScreenLayout starts for " + msg);
+    }
+    else
+    {
+        msg = "null contest";
+        traceMsg("clearScreenLayout starts for " + msg);
+    }
 
     qsoModel.initialise(nullptr);
 
@@ -873,6 +882,11 @@ void TSingleLogFrame::on_ContestPageChanged ()
 {
     if (!ui)
         return;
+
+    if (contest == nullptr)
+    {
+        return;
+    }
 
     if ( this != LogContainer->getCurrentLogFrame() )
     {
