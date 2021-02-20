33cd304c188841ffa42849f317f44ed4af85d22d
Mike Goodey <mike@goodey.org.uk>
2021-02-20 08:51:35 +0000

    We don't need to create the logs directory - QFile::open does it for us

--- mqt/MqtLogger/LoggerContest.cpp
+++ mqt/MqtLogger/LoggerContest.cpp
@@ -371,7 +371,7 @@ bool LoggerContestLog::initialise( const QString &fn, bool newFile, int slotno )
    else
       if ( minosFile )
       {
-          CreateDir(fn);
+          //CreateDir(fn);
           QIODevice::OpenMode om = QIODevice::ReadWrite | QIODevice::Unbuffered;
           if (isUnwriteable())
           {
