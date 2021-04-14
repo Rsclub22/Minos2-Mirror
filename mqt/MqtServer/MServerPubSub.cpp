74ceaeea799a7c19c5f9e573cba7394b5b4061b5
Mike Goodey <mike@goodey.org.uk>
2021-04-14 15:12:40 +0100

    test corrupted data

--- mqt/MqtServer/MServerPubSub.cpp
+++ mqt/MqtServer/MServerPubSub.cpp
@@ -380,7 +380,7 @@ void Published::buildPublishedTree(QTreeWidget *tree)
 {
     tree->clear();
     tree->setColumnCount(4);
-    QStringList h = {tr("key"), tr("router"), tr("state"), tr("value")};
+    QStringList h = {tr("key"), tr("router"), tr("pubid"), tr("state"), tr("value")};
     tree->setHeaderLabels(h);
     for ( auto const &p: qAsConst(Published::publist) )
     {
@@ -393,12 +393,14 @@ void Published::buildPublishedTree(QTreeWidget *tree)
            QString value = j->getPubValue();
            PublishState state = j->getPubState();
            QString router = j->getRouter();
+           QString pubid = j->getPubId();
 
            QTreeWidgetItem *keyItem = new QTreeWidgetItem(catItem);
            keyItem->setText(0, key);
            keyItem->setText(1, router);
-           keyItem->setText(2, stateList[state]);
-           keyItem->setText(3, value);
+           keyItem->setText(2, pubid);
+           keyItem->setText(3, stateList[state]);
+           keyItem->setText(4, value);
         }
 
     }
@@ -667,7 +669,7 @@ void PublishedCategory::routerPublish( const QString &pubId, const QString &svr,
       pubkeylist.push_back( p );
       kl = findPubKey( svr, pubId, k );
       PublishedKeyListIterator ikl = pubkeylist.end();
-      if (++kl != ikl)
+      if (kl != --ikl)
       {
           trace(QString("Bad PublishedKey creation: %1 %2 %3 %4").arg(category, svr, pubId, v));
       }
