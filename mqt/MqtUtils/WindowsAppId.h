9cf3af23e5876873d40fe867e1e0c835c0d02124
Mike Goodey <mike@goodey.org.uk>
2021-08-27 19:27:25 +0100

    and again

--- mqt/MqtUtils/WindowsAppId.h
+++ mqt/MqtUtils/WindowsAppId.h
@@ -1,10 +1,10 @@
 #ifndef WINDOWSAPPID_H
 #define WINDOWSAPPID_H
 
-#include <QtGlobal>
 #include <QWidget>
+#ifdef Q_OS_WIN
 #include <windows.h>
-
+#endif
 void setWinAppId(QWidget *w, QString id);
 void clearWinAppId(QWidget *w);
 #endif // WINDOWSAPPID_H
