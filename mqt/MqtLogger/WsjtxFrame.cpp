655602e029753c09d17eef835d4e8aa7e352b0ac
Mike Goodey <mike@goodey.org.uk>
2022-07-08 15:09:21 +0100

    If my call is clicked, transfer the other call

--- mqt/MqtLogger/WsjtxFrame.cpp
+++ mqt/MqtLogger/WsjtxFrame.cpp
@@ -1113,49 +1113,30 @@ void WsjtxFrame::on_configCQButton_clicked()
 
 void WsjtxFrame::on_decodes_table_view__clicked(const QModelIndex &index)
 {
+    // How do we say "use from call"?
+
     if (index.column() == dcMessage)
     {
-        QString call;
+        Callsign call;
         decodeMessage &dc = messages[index.row()];
         switch (dc.mstage)
         {
         case emsNone:
             break;
-        case emsCQ:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsGrid:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsDb:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsDbGrid:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsRplusGrid:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsRplusDb:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsRplusDbGrid:
-            call = dc.fromCall.getFullCall();
-            break;
-        case emsRRR:
-            call = dc.fromCall.getFullCall();
-            break;
-        case ems73:
-            call = dc.fromCall.getFullCall();
-            break;
         case emsFree:
             break;
-
+        default:
+            call = dc.fromCall;
+            break;
         }
-        if (!call.isEmpty())
+        if (call == decoder.getMyCall())
+        {
+            call = dc.toCall;
+        }
+        if (!call.getFullCall().isEmpty())
         {
             TSingleLogFrame *tslf = LogContainer->getCurrentLogFrame();
-            tslf->transferFromWSJTX(call);
+            tslf->transferFromWSJTX(call.getFullCall());
         }
     }
 }
