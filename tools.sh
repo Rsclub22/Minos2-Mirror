6049254934ff64e8914c356ab9fbe5c34cd6f32a
Mike Goodey <mike@goodey.org.uk>
2022-07-20 19:31:37 +0100

    Add -y tp g++

--- tools.sh
+++ tools.sh
@@ -18,7 +18,7 @@ apt-get install qttools5-dev -y
 
 # ubuntu 21 doesn't seem to have g++ by default
 
-apt-get install g++
+apt-get install g++ -y
 apt-get install libasound2 -y
 apt-get install libasound2-dev -y
 
