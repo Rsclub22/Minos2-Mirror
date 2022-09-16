a47bcbc83fcebe06b79c7c58dc43000ab2f2c462
Mike Goodey <mike@goodey.org.uk>
2022-09-16 15:19:12 +0100

    Allow for bad dtg from cluster (Sept and Sep problem)

--- mqt/MqtLogger/bandmapclientframe.cpp
+++ mqt/MqtLogger/bandmapclientframe.cpp
@@ -920,6 +920,10 @@ QSharedPointer<BandmapSpotData> BandmapClientFrame::stringToDxSpot(QString spot)
 
             spotDateTime = QDateTime::fromString(spotlist[SPOTDATETIME], "yyyyMMMddHHmmss" );
             spotDateTime.setTimeSpec(Qt::UTC);
+            if (!spotDateTime.isValid())
+            {
+                spotDateTime = QDateTime::currentDateTimeUtc();
+            }
             qint64 rxTime = spotDateTime.toMSecsSinceEpoch() / 1000;
 
             traceMsg(QString("Add Cluster Spot to Bandmap %1, %2, %3, %4").arg(spotlist[DXCALL], spotlist[DXFREQ], spotlist[DXMODESTR], spotlist[DXLOCATOR]));
