2a8e0debf4553ef429e1946d34b5027d9d6bac84
Mike Goodey <mike@goodey.org.uk>
2022-12-09 17:01:00 +0000

    Didn't target 23 for xml
    (cherry picked from commit 7f3821a5ce7de4a5e4b94095df04781c33681546)

--- mqt/Installer/buildInstall.bat
+++ mqt/Installer/buildInstall.bat
@@ -100,13 +100,13 @@ cd Configuration
 
 call powershell.exe "& {Invoke-WebRequest https://www.country-files.com/cty/cty.dat -Outfile cty.dat}"
 call powershell.exe "& {Invoke-WebRequest https://www.rsgbcc.org/vhf/vhfcontests22.xml -Outfile vhfcontests22.xml}"
-call powershell.exe "& {Invoke-WebRequest https://www.rsgbcc.org/vhf/vhfcontests23.xml -Outfile vhfcontests21.xml}"
+call powershell.exe "& {Invoke-WebRequest https://www.rsgbcc.org/vhf/vhfcontests23.xml -Outfile vhfcontests23.xml}"
 
 call powershell.exe "& {Invoke-WebRequest http://www.rsgbcc.org/vhf/hfcontests22.xml -Outfile hfcontests22.xml}"
-call powershell.exe "& {Invoke-WebRequest http://www.rsgbcc.org/vhf/hfcontests23.xml -Outfile hfcontests21.xml}"
+call powershell.exe "& {Invoke-WebRequest http://www.rsgbcc.org/vhf/hfcontests23.xml -Outfile hfcontests23.xml}"
 
 call powershell.exe "& {Invoke-WebRequest http://microwave.rsgbcc.org/microcontests22.xml -Outfile microcontests22.xml}"
-call powershell.exe "& {Invoke-WebRequest http://microwave.rsgbcc.org/microcontests23.xml -Outfile microcontests21.xml}"
+call powershell.exe "& {Invoke-WebRequest http://microwave.rsgbcc.org/microcontests23.xml -Outfile microcontests23.xml}"
 
 
 cd ../Bin
