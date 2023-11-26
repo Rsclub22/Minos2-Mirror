a503509435f93906f02a4af7b3e5e46de058734c
Michael Goodey <mjg@Mac-mini-2.broadband>
2023-11-26 16:37:10 +0000

    Clean up build/install

--- mactools.sh
+++ mactools.sh
@@ -7,7 +7,7 @@ brew upgrade -y
 brew install git 
 # brew install git-gui -y
 brew install wget
-brew install cmake autoconf# /usr/local/Cellar/hamlib/1.2.15.3
+brew install cmake autoconf
 
 brew install libasound2 -y
 brew install libasound2-dev -y
