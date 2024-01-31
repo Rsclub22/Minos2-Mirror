c6af977d97d6f0e0c01263529b135aaa870d14e0
Mike Goodey <mike@goodey.org.uk>
2024-01-31 10:51:04 +0000

    Preserve permissions when copying shell scripts

--- mqt/Installer/buildInstall.sh
+++ mqt/Installer/buildInstall.sh
@@ -95,8 +95,8 @@ cp -r $DIR/mqt/ControlFiles/Configuration/LinuxFiles/* ./Configuration
 rm -rf ./Configuration/WindowsFiles
 rm -rf ./Configuration/LinuxFiles
 rm -rf ./Configuration/MacFiles
-cp $DIR/mqt/Installer/Minos.sh . 
-cp $DIR/mqt/Installer/runAppStarter.sh .
+cp -p $DIR/mqt/Installer/Minos.sh . 
+cp -p $DIR/mqt/Installer/runAppStarter.sh .
 cp $DIR/mqt/Docs/*.* ./Docs
 cp $DIR/mqt/*.ico .
 
@@ -136,8 +136,8 @@ if [ ! -d ~/$RUNTIME/Lists ]; then
 fi
 cp -rv Bin ~/$RUNTIME
 cp *.ico ~/$RUNTIME
-cp runAppStarter.sh ~/$RUNTIME
-cp Minos.sh ~/$RUNTIME
+cp -p runAppStarter.sh ~/$RUNTIME
+cp -p Minos.sh ~/$RUNTIME
 
 read -n 1 -p "Do you want to copy the configuration to ~/$RUNTIME (press y/n)? " ans;
 
