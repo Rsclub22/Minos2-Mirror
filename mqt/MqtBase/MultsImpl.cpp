259748523287f3e705469515c582a7622548c428
Mike Goodey <mike@goodey.org.uk>
2024-02-11 14:39:46 +0000

    Remove multlist diagnostic dump

--- mqt/MqtBase/MultsImpl.cpp
+++ mqt/MqtBase/MultsImpl.cpp
@@ -850,7 +850,7 @@ bool MultListsImpl::loadMultFiles( )
    distList.load();
    distSynList.load();
    glist.load();
-// /*
+ /*
    QFile fos("c:/temp/multlist.txt");
    if (!fos.open(QIODevice::WriteOnly|QIODevice::Text))
       return false;
@@ -887,7 +887,7 @@ bool MultListsImpl::loadMultFiles( )
    {
       os << i->wt->synPrefix + " : " + i->wt->dupPrefix + "\n";
    }
-// */
+ */
    return true;
 }
 MultListsImpl::MultListsImpl()
