cda924d8ee9aff34f36d0ba433712bae41e1ee23
Mke Goodey <mike@goodey.org.uk>
2025-10-04 17:28:07 +0100

    Try to get distance and bearing for cluster spots

--- mqt/MqtLogger/bandmapclientframe.cpp
+++ mqt/MqtLogger/bandmapclientframe.cpp
@@ -1047,6 +1047,17 @@ void BandmapClientFrame::addDxSpotToBandmapTable(QSharedPointer<ClusterSpotData>
         newSpot->setDxLocator(loc);
     }
 
+    QString distance;
+    QString dxBrg;
+    if (!newSpot->getDxLocator().isEmpty())
+    {
+        double dist = 0;
+        int brg = 0;
+        ct->calcDistanceBearing(newSpot->getDxLocator(), &dist, &brg);
+        distance = QString::number(static_cast<int>(dist));
+        dxBrg = QString::number(brg);
+    }
+
     qint64 logTime = newSpot->getSpotDateTime().toMSecsSinceEpoch() / 1000;
     //QString logTimeStr = spot->getSpotDateTime().time().toString("HH:mm");
     newSpot->setRxTime(logTime);
